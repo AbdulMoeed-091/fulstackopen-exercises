980385b9e5870fd0b3a5ceb3a42b1e994aa86e0d
Moeed Masood <chmoeed04@gmail.com>
2023-02-16 15:37:55 +0500

    props

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,22 @@
 import { useState } from "react";
 
+
+const Statistics=(props)=>{
+return(
+<>
+<p>good {props.good}</p>
+<p>neutral {props.neutral}</p>
+<p>bad {props.bad}</p>
+<p>all {props.all}</p>
+<p>average {props.average}</p>
+<p>positive {props.positive}%</p>
+</>
+
+)
+
+}
+
+
 const App = () => {
 
   const [good, setGood] = useState(0);
@@ -13,18 +30,24 @@ function neutralInc(){ return(setNeutral(neutral+1))}
 
 function badInc(){ return(setBad(bad+1))}
 
+const all = good+neutral+bad
+const average = all/3
+const positive = (good/all)*100
 
   return (
     <div>
       <h1>Give Feedback !</h1>
-
       <button onClick={goodInc}>Good</button> <b/>
       <button onClick={neutralInc}>Neutral</button> <b/>
       <button onClick={badInc}>Bad</button> <b/>
-<h2>Statistics</h2>
-      <p>Good {good} </p>
-      <p>Neutral {neutral} </p>
-      <p>Bad {bad} </p>
+      <h2>Statistics</h2>
+      <Statistics 
+      good={good} 
+      neutral={neutral} 
+      bad={bad} 
+      all={all} 
+      average={average}
+      positive={positive} />
     </div>
   );
 }
