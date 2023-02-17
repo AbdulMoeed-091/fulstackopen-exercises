52780a919d3c6c47318337e162a424573308e2d4
Moeed Masood <chmoeed04@gmail.com>
2023-02-17 15:30:27 +0500

    prop added

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -3,23 +3,29 @@ const Course=(props)=>{
 
   const {course} = props;
 
+  
+  const total = course.parts.reduce((s, p) => {
+    console.log(s, p)
+    return s+p},0)
 
   return(
 
     <div>
 <h1>{course.name}</h1>
-<div>{course.parts.map(values=>{
-  return(
+<div>{course.parts.map(values=>{return(
+    <p key={values.id}> {values.name}{values.exercises} </p> 
+    
+  )
+    })}
+    
+    
+  <h5> total of {total} exercises</h5>
   
-    <p key={values.id}> {values.name}{values.exercises} </p>
-    
-    )}
-    
-    )
-    }</div>
+  </div>
 
     </div>
   )
+  
     
 
 }
