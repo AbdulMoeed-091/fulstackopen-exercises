17c003a4c0fd494c6534b26ba9140ffd38a4fb24
Moeed Masood <chmoeed04@gmail.com>
2023-02-17 12:18:21 +0500

    phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,16 @@
+
+const App = (props) => {
+
+  const {notes} = props;
+
+  return (
+    <div>
+      <h1>Notes</h1>
+{notes.map(note=><li key={note.id}>{note.content}</li>)}
+
+
+    </div>
+  );
+}
+
+export default App;
