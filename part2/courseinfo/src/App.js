75e17bae88834b38761a495066aba98a527f79cf
Moeed Masood <chmoeed04@gmail.com>
2023-02-17 12:57:28 +0500

    passing data through props

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -2,17 +2,30 @@ import Course from "./Course";
 
 
 
-const App = (props) => {
-
-  const {notes} = props;
-
-  return (
-    <div>
-      <h1>Notes</h1>
-{notes.map(note=><li>{note.content}</li>)}
-
-
-    </div>
+const App = () => {
+  
+  const course = {
+  id: 1,
+  name: 'Half Stack application development  ',
+  parts: [
+    {
+      name: 'Fundamentals of React  ',
+      exercises: 10,
+      id: 1
+    },
+    {
+      name: 'Using props to pass data  ',
+      exercises: 7,
+      id: 2
+    },
+    {
+      name: 'State of a component  ',
+      exercises: 14,
+      id: 3
+    }
+  ]
+}
+  return ( <Course name={course.name} course={course}/>
   );
 }
 
--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,26 +1,26 @@
 
-const Course=()=>{
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-          }
-        ]
-      }
+const Course=(props)=>{
+
+  const {course} = props;
+
+
+  return(
+
+    <div>
+<h1>{course.name}</h1>
+<div>{course.parts.map(values=>{
+  return(
+  
+    <p key={values.id}> {values.name}{values.exercises} </p>
+    
+    )}
+    
+    )
+    }</div>
+
+    </div>
+  )
+    
 
 }
 
