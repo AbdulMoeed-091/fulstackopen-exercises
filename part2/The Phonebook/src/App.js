bed017430776c263aed67d9ac48e09fbad7b2e95
Moeed Masood <chmoeed04@gmail.com>
2023-02-22 10:49:32 +0500

    filter

--- part2/The Phonebook/src/App.js
+++ part2/The Phonebook/src/App.js
@@ -4,7 +4,8 @@ import Note from "./note";
 const App = (props) => {
 
   const [notes, setNotes] = useState(props.notes)
-const [newNote, setNewNote] = useState([])
+const [newNote, setNewNote] = useState([]);
+const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault()
@@ -12,25 +13,30 @@ const [newNote, setNewNote] = useState([])
       id: notes.length+1,
       content: newNote,
       important: Math.random()<0.5
-    }
-    
+    }    
   setNotes(notes.concat(noteObject))
   setNewNote('')
   }
 
+  const notesToShow = showAll?notes:notes.filter(note => note.important ===true)
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
 
-{notes.map(note=><Note key={note.id} note={note}/>
+{/* {notes.map(note=><Note key={note.id} note={note}/>
+)} */}
+{notesToShow.map(note=><Note key={note.id} note={note}/>
 )}
-
       </ul>
 <form onSubmit={addNote}>
 <input value={newNote} onChange={handleNoteChange} placeholder="Enter note here!"/>
