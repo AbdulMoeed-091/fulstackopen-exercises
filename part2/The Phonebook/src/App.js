3b28a5248cd62619550ad4bfeaa3536ffb5dc2ff
Moeed Masood <chmoeed04@gmail.com>
2023-02-22 09:58:04 +0500

    new note added

--- part2/The Phonebook/src/App.js
+++ part2/The Phonebook/src/App.js
@@ -4,13 +4,21 @@ import Note from "./note";
 const App = (props) => {
 
   const [notes, setNotes] = useState(props.notes)
-const [newNote, setNewNote] = useState('Hi, I am new note')
+const [newNote, setNewNote] = useState([])
 
   const addNote = (event) => {
     event.preventDefault()
-    console.log("Note Added", event.target)
-
+    const noteObject = {
+      id: notes.length+1,
+      content: newNote,
+      important: Math.random()<0.5
+    }
+    
+  setNotes(notes.concat(noteObject))
+  setNewNote('')
   }
+
+
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
@@ -25,7 +33,7 @@ const [newNote, setNewNote] = useState('Hi, I am new note')
 
       </ul>
 <form onSubmit={addNote}>
-<input value={newNote} onChange={handleNoteChange}/>
+<input value={newNote} onChange={handleNoteChange} placeholder="Enter note here!"/>
 <button type="submit">Submit</button>
 </form>
     </div>
